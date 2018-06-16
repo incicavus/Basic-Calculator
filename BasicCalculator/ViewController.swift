c387821160fe79a46f20997377eee351baac3721
İnci Çavuş <incicavus90@gmail.com>
2018-06-16 16:38:28 +0300

    first commit

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextX: UITextField!
+    @IBOutlet weak var TextY: UITextField!
+    @IBOutlet weak var LabelSonuc: UILabel!
+    var result = 0
+    
+    @IBAction func Sum(_ sender: Any) {
+        if let firstNumber = Int(TextX.text!) {
+            if let secondNumber = Int(TextY.text!) {
+            result = firstNumber + secondNumber
+                LabelSonuc.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func Minus(_ sender: Any) {
+        if let firstNumber = Int(TextX.text!) {
+            if let secondNumber = Int(TextY.text!) {
+                 result = firstNumber - secondNumber
+                LabelSonuc.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func Multiply(_ sender: Any) {
+        if let firstNumber = Int(TextX.text!) {
+            if let secondNumber = Int(TextY.text!) {
+                 result = firstNumber * secondNumber
+                LabelSonuc.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func Divided(_ sender: Any) {
+        if let firstNumber = Int(TextX.text!) {
+            if let secondNumber = Int(TextY.text!) {
+                 result = firstNumber / secondNumber
+                LabelSonuc.text = String(result)
+            }
+        }
+    }
+    
+    
+    //uygulamamız ilk açıldığında ne olmasını istiyorsak buraya yazıyoruz.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        LabelSonuc.text = ""
     }
 
+    //Silinebilir, eğer hafıza problemi yaşarsak ne olacağı ile alakalı fonksiyon
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
